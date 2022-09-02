e491c5a6c4442aea9dd8566fe5af64a73601c719
Abhi-exl <108261156+Abhi-exl@users.noreply.github.com>
2022-09-02 21:25:10 +0530

    Create test_abhi.py

---
+++ test_abhi.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+#pd.set_option('max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', -1)
+
